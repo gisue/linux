f56ed0e311528badf302c113e42da1c24aedf087
peppow <155969124+gisue@users.noreply.github.com>
2024-03-30 17:22:11 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -4,30 +4,30 @@
 sudo pacman -Syu
 
 # Install desired packages
-sudo pacman -S git btop htop vim neovim ranger neofetch cowsay cmatrix sl lolcat
+sudo pacman -S git btop neovim ranger neofetch cowsay cmatrix sl lolcat
 
 # Create programs folder if it doesn't exist
-mkdir -p programs
+mkdir -p Programs
 
 # Install Pokemon Colorscripts
-git clone https://gitlab.com/phoneybadger/pokemon-colorscripts.git programs/pokemon-colorscripts
-cd programs/pokemon-colorscripts
+git clone https://gitlab.com/phoneybadger/pokemon-colorscripts.git Programs/pokemon-colorscripts
+cd Programs/pokemon-colorscripts
 sudo ./install.sh
 cd ../..  # Navigate back to original directory
 
 # Install Shell Color Scripts
-git clone https://gitlab.com/dwt1/shell-color-scripts.git programs/shell-color-scripts
-cd programs/shell-color-scripts
+git clone https://gitlab.com/dwt1/shell-color-scripts.git Programs/shell-color-scripts
+cd Programs/shell-color-scripts
 sudo make install
 cd ../..
 
 # Install Cbonsai (commented out, uncomment if desired)
-git clone https://gitlab.com/jallbrit/cbonsai programs/cbonsai
-cd programs/cbonsai
+git clone https://gitlab.com/jallbrit/cbonsai Programs/cbonsai
+cd Programs/cbonsai
 sudo make install
 cd ../..
 
 # Install Pipe.sh (commented out, follow project instructions)
-git clone https://github.com/pipeseroni/pipes.sh.git programs/pipe.sh
+git clone https://github.com/pipeseroni/pipes.sh.git Programs/pipe.sh
 
 echo "Installation complete!"
